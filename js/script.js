117b1efc6a48c9ce2dc136ee0af45e8bbf25fc07
krithika089 <krithikakumari99@gmail.com>
2020-08-04 13:10:34 +0530

    Web UI

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function validate(){
+	var nme=document.getElementById("menu-name");
+var price=document.getElementById("numbe").value;
+var date=document.getElementById("date").value;
+	
+	if(price.length!==0)
+	{ alert("Fill all the required details");
+		if(nme.length!==0)
+		{ alert("Fill all the required details");
+			if(date.length!==null)
+	
+	alert("Fill all the required details");
+	}
+	}
+	return false;
+	
+	}
